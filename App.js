06c6f44976cddd40368ebc258d971563bb3af91c
jit0908 <ne0001913@gmail.com>
2022-04-17 16:32:44 +0900

    Todo 앱 init

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>TodoApp</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
