825c7bc9ce8b4f031e0d33ff8ad10185508afce6
jit0908 <ne0001913@gmail.com>
2022-04-17 16:44:05 +0900

    저장 시 prettier 적용되도록 설정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
